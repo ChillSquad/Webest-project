701df52de7b508611f61fe2fbaebe7f5a3c8b821
tunka8090 <tunka8090@yandex.ru>
2024-08-14 22:32:51 +0300

    [add]: Создание компонента navigation

---
+++ components/Header/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <ul>
+    <li><a href="#">Услуги</a></li>
+    <li><a href="#">Кейсы</a></li>
+    <li><a href="#">Блог</a></li>
+    <li><a href="#">Компания</a></li>
+    <li><a href="#">Контакты</a></li>
+  </ul>
+</template>
