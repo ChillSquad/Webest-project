63ca59fa9b55e3c3f44ac1b6a5b4740f8828db14
tunka8090 <tunka8090@yandex.ru>
2024-08-17 22:52:30 +0300

    [upd]: Добавление адаптивности

--- components/Header/MobileMenu.vue
+++ components/Header/MobileMenu.vue
@@ -1,15 +1,22 @@
 <script setup>
-import { isShowMobileMenu } from "../models/header"; // Импорт состояния видимости
+import { isShowMobileMenu } from "../models/header";
 import Navigation from "./Navigation.vue";
+import Contacts from "./Contacts.vue";
 </script>
 
 <template>
   <Transition name="fade" mode="out-in">
-    <div v-if="isShowMobileMenu">
-      <!-- Показываем меню только если isShowMobileMenu = true -->
-      <Navigation />
+    <div class="mobile-menu" v-if="isShowMobileMenu">
+      <div class="container">
+        <div class="mobile-menu__inner">
+          <div class="mobile-menu__nav">
+            <h3><Navigation /></h3>
+          </div>
+          <div class="mobile-menu__contscts">
+            <Contacts phone="+7 (495) 128-22-90" mail="info@webest.ru" />
+          </div>
+        </div>
+      </div>
     </div>
   </Transition>
 </template>
-
-<style scoped></style>
--- components/Header/Navigation.vue
+++ components/Header/Navigation.vue
@@ -1,9 +1,11 @@
 <template>
-  <ul>
-    <li><a href="#">Услуги</a></li>
-    <li><a href="#">Кейсы</a></li>
-    <li><a href="#">Блог</a></li>
-    <li><a href="#">Компания</a></li>
-    <li><a href="#">Контакты</a></li>
-  </ul>
+  <nav>
+    <ul>
+      <li><a href="#">Услуги</a></li>
+      <li><a href="#">Кейсы</a></li>
+      <li><a href="#">Блог</a></li>
+      <li><a href="#">Компания</a></li>
+      <li><a href="#">Контакты</a></li>
+    </ul>
+  </nav>
 </template>
