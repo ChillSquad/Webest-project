c008be97a2d908a993355dd541711a6e42639e30
tunka8090 <tunka8090@yandex.ru>
2024-08-17 23:35:43 +0300

    [upd]: Добавление компонента footer

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup>
+import Header from "./components/Header/Header.vue";
+import MobileMenu from "./components/Header/MobileMenu.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
 <template>
-  <div>
-    <Header />
+  <div class="wrapper">
+    <div class="main-header">
+      <Header />
+    </div>
+    <div class="main-mobile">
+      <MobileMenu />
+    </div>
+    <div class="main-footer">
+      <Footer />
+    </div>
   </div>
 </template>
