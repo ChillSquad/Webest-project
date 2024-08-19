9b2e4570fe7ef46f1ec9028def60c87036c0a2b4
tunka8090 <tunka8090@yandex.ru>
2024-08-19 22:03:47 +0300

    [upd]: Добавление компанента first-page

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
 import Header from "./components/Header/Header.vue";
 import MobileMenu from "./components/Header/MobileMenu.vue";
 import Footer from "./components/Footer.vue";
+import FirsPage from "./components/FirstPage.vue";
 </script>
 
 <template>
@@ -9,9 +10,15 @@ import Footer from "./components/Footer.vue";
     <div class="main-header">
       <Header />
     </div>
+
     <div class="main-mobile">
       <MobileMenu />
     </div>
+
+    <div class="main-first-page">
+      <FirsPage />
+    </div>
+
     <div class="main-footer">
       <Footer />
     </div>
