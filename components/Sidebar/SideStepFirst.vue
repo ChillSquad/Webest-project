065e1bf7686fd9dbb60d9e1b67a8e452d30c6397
tunka8090 <tunka8090@yandex.ru>
2024-08-22 23:59:55 +0300

    [add]: Создание еомпонентов sidebar

---
+++ components/Sidebar/SideStepFirst.vue
@@ -0,0 +1,47 @@
+<script setup>
+import GradientButton from "../UI-kit/GradientButton.vue";
+
+import iconPaperClip from "../icons/iconPaperClip.vue";
+
+import InputText from "primevue/inputtext";
+import Textarea from "primevue/textarea";
+</script>
+
+<template>
+  <div class="sidebar-content">
+    <div class="sidebar-input">
+      <div class="sidebar-input__item">
+        <label for="username">Как вас зовут</label>
+        <InputText class="sidebar-input__field" id="username" v-model="value" />
+      </div>
+
+      <div class="sidebar-input__item">
+        <label for="mail">Почта, телефон или мессенджер</label>
+        <InputText class="sidebar-input__field" id="mail" v-model="value" />
+      </div>
+
+      <div class="sidebar-input__item">
+        <label for="task">Кратко опишите задачу</label>
+        <Textarea
+          autoResize
+          rows="1"
+          class="sidebar-input__field"
+          id="task"
+          v-model="value"
+        />
+      </div>
+
+      <div class="sidebar-input__file">Прикрепить файл <iconPaperClip /></div>
+    </div>
+
+    <div class="sidebar-footer">
+      <GradientButton title="Оставить заявку" />
+      <span class="sidebar-footer__privacy"
+        >Нажимая кнопку, вы соглашаетесь с нашей
+        <a class="sidebar-footer__privacy-link" href="#"
+          >политикой конфиденциальности</a
+        ></span
+      >
+    </div>
+  </div>
+</template>
---
+++ components/Sidebar/SideStepThird.vue
@@ -0,0 +1,49 @@
+<script setup>
+import GradientButton from "../UI-kit/GradientButton.vue";
+</script>
+
+<template>
+  <div class="sidebar-content">
+    <div class="sidebar-success">
+      <div class="sidebar-success__massage">
+        <span class="sidebar-success__uppercase">Что-то пошло не так</span>
+        <span class="sidebar-success__span"
+          >Попробуйте еще раз или вернитесь позже</span
+        >
+      </div>
+      <GradientButton title="Повторить" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.sidebar-content {
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 62px;
+  .sidebar-success {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+    &__massage {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 16px;
+    }
+    &__uppercase {
+      text-transform: uppercase;
+      font-family: "Montserrat-Bold";
+      font-size: 28px;
+    }
+    &__span {
+      font-size: 18px;
+    }
+  }
+}
+</style>
