c1537c4892c6a910ed9cc2ac359c2116d0262fab
tunka8090 <tunka8090@yandex.ru>
2024-08-29 21:40:04 +0300

    [add]: Создание компонента TalkPage

---
+++ components/TalkPage/TalkPageCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: null,
+  },
+  link: {
+    type: String,
+    default: null,
+  },
+});
+import iconTitleArrowRight from "../icons/iconTitleArrowRight.vue";
+</script>
+
+<template>
+  <a :href="`${link}`" class="talk-card">
+    <div class="talk-card__inner">
+      <div class="talk-card__content">
+        <h6 class="talk-card__heading">
+          {{ title }}
+        </h6>
+        <div class="talk-card__arrow">
+          <iconTitleArrowRight />
+        </div>
+      </div>
+    </div>
+  </a>
+</template>
