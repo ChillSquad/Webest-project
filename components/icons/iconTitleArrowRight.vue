66149c30932435537c4abd5f694e8bbaac2d9a80
tunka8090 <tunka8090@yandex.ru>
2024-08-29 21:35:03 +0300

    [add]: Добавление иконок

---
+++ components/icons/iconTitleArrowRight.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    class="title-arrow-right"
+    width="54"
+    height="52"
+    viewBox="0 0 54 52"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M40.905 22.6663L23.025 4.7863L27.739 0.0722656L53.6663 25.9997L27.739 51.9267L23.025 47.2127L40.905 29.333H0.333008V22.6663H40.905Z"
+      fill="#000309"
+    />
+  </svg>
+</template>
