5752d093ae7270303d30dfde8d74d21f19749c47
tunka8090 <tunka8090@yandex.ru>
2024-08-29 21:36:23 +0300

    [add]: Создание компонента CasePage

---
+++ components/CasePage/CasePagePlate.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  items: Array,
+  platePosition: {
+    type: String,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div :class="['case-page-plate', { position: platePosition }]">
+    <div class="case-page-plate__inner">
+      <div
+        v-for="(item, index) in items"
+        :key="item.id"
+        class="case-page-plate__box"
+      >
+        <div class="case-page-plate__content">
+          <span class="case-page-plate__percent"
+            >{{ item.sign }}{{ item.percent }}%</span
+          >
+          <span class="case-page-plate__text">{{ item.text }}</span>
+        </div>
+
+        <hr class="case-page-plate__line" v-if="index === 0 || index === 1" />
+      </div>
+    </div>
+  </div>
+</template>
