b9624108152c4431070233a3e759b9a068bca597
tunka8090 <tunka8090@yandex.ru>
2024-08-30 21:21:02 +0300

    [add]: Создание компонента AboutCompany

---
+++ components/AboutCompany/AboutCompanyContent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TitleButton from "../UI-kit/TitleButton.vue";
+</script>
+
+<template>
+  <div class="about-company-content">
+    <div class="about-company-content__inner">
+      <div class="about-company-content__heading">
+        <TitleButton
+          title="кратко о webest"
+          class="about-company-content__title"
+        />
+        <div class="about-company-content__heading-subtitle">
+          Узнайте больше о нашей
+          <span class="custom-standing-purple">команде</span> и последних
+          <span class="custom-standing-yellow">новостях</span> компании
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
