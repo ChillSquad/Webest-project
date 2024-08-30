f8d491fdf5c37a79b9a6a65e560e21aacb5f1985
tunka8090 <tunka8090@yandex.ru>
2024-08-30 21:20:11 +0300

    [upd]: Изменение компонентов UI-kit

--- components/UI-kit/TitleButton.vue
+++ components/UI-kit/TitleButton.vue
@@ -10,29 +10,44 @@ defineProps({
 
 <template>
   <a href="#" class="title-button">
-    <h2 class="title-button__heading">{{ title }}</h2>
-    <div class="super">
-      <iconTitleArrowRight />
-    </div>
+    <h2 id="title-button-class-management" class="title-button__heading">
+      {{ title }}
+    </h2>
+    <iconTitleArrowRight />
   </a>
 </template>
 
 <style lang="scss" scoped>
 .title-button {
   display: flex;
-  gap: 20px;
+  gap: 21.33px;
   align-items: center;
   color: var(--color-black);
   width: fit-content;
+  transition: gap 0.3s ease-in-out;
+  &:hover {
+    gap: 29.33px;
+  }
   &__heading {
     font-family: "Montserrat-Bold";
     font-size: 80px;
+    line-height: 96px;
     text-transform: uppercase;
+    transition: color 0.3s ease-in-out;
+    &:hover {
+      color: var(--color-blue);
+    }
   }
-
+  .title-arrow-right {
+    transition: fill 0.3s ease-in-out;
+  }
+  &:hover .title-arrow-right {
+    fill: var(--color-blue);
+  }
   @media (max-width: 800px) {
     &__heading {
       font-size: 32px;
+      line-height: 36.48px;
     }
     .title-arrow-right {
       width: 27px;
