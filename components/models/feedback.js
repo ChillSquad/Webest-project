08e3322e4fff3b0b70247c1c2d30485d36338db0
tunka8090 <tunka8090@yandex.ru>
2024-09-06 22:23:38 +0300

    [upd]: Изменение стилей

--- components/models/feedback.js
+++ components/models/feedback.js
@@ -1,31 +1,37 @@
+import { ref } from "vue";
+
 const useFeedbackFormModel = () => {
   const activeFormStep = ref(0);
 
   const fields = ref({
     name: "",
     email: "",
+    task: "",
   });
-
-  const nextStep = () => {
-    activeFormStep.value++;
-  };
 
   const submit = async () => {
     try {
-      const response = await $fetch("/mail.php", {
-        method: "POST",
-        body: JSON.parse(JSON.stringify(fields.value)),
-      });
+      const response = await $fetch(
+        "https://94fc376960523c1c.mokky.dev/tasks",
+        {
+          method: "POST",
+          body: fields.value,
+        }
+      );
 
       if (response) {
+        // Если запрос успешен, переходим на шаг 1
+        activeFormStep.value = 1;
       }
-    } catch (error) {}
+    } catch (error) {
+      // Если произошла ошибка, переходим на шаг 2
+      activeFormStep.value = 2;
+    }
   };
 
   return {
     activeFormStep,
-
-    nextStep,
+    fields,
     submit,
   };
 };
