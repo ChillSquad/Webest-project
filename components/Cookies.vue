bb5e2d781bd1ca4c4b7ee985cbfb6f5a42d5f988
tunka8090 <tunka8090@yandex.ru>
2024-09-07 22:10:42 +0300

    [add]: Создание компонента Cookies

---
+++ components/Cookies.vue
@@ -0,0 +1,13 @@
+<script setup>
+import GradientButton from "./UI-kit/GradientButton.vue";
+</script>
+<template>
+  <div class="cookies">
+    <div class="cookies__heading">
+      Мы используем куки, чтобы пользоваться сайтом было удобно
+    </div>
+    <span>
+      <GradientButton title="Отлично" />
+    </span>
+  </div>
+</template>
