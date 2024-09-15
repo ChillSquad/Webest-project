c117e086414775925b9542f3a64339a1074034fe
tunka8090 <tunka8090@yandex.ru>
2024-09-15 19:30:58 +0300

    [add]: Создание запроса сотрудника

---
+++ components/Sidebar/SideStepFirstStaff.vue
@@ -0,0 +1,67 @@
+<script setup>
+import GradientButton from "../UI-kit/GradientButton.vue";
+import FileUploadCustom from "../UI-kit/FileUploadCustom.vue";
+import InputText from "primevue/inputtext";
+import Textarea from "primevue/textarea";
+
+import { useFeedbackFormModelStaff } from "../models/feedbackStaff";
+const { fieldsStaff, errorsStaff, submitStaff } = useFeedbackFormModelStaff();
+</script>
+
+<template>
+  <div class="sidebar-content">
+    <div class="sidebar-input">
+      <div class="sidebar-input__item">
+        <label for="username">Как вас зовут</label>
+        <InputText
+          class="sidebar-input__field"
+          :class="{ 'input-error': errorsStaff.name }"
+          id="username"
+          v-model="fieldsStaff.name"
+        />
+        <span v-if="errorsStaff.name" class="sidebar-input__error-message"
+          >Это поле обязательно</span
+        >
+      </div>
+
+      <div class="sidebar-input__item">
+        <label for="mail">Почта, телефон или мессенджер</label>
+        <InputText
+          class="sidebar-input__field"
+          :class="{ 'input-error': errorsStaff.email }"
+          id="mail"
+          v-model="fieldsStaff.email"
+        />
+        <span v-if="errorsStaff.email" class="sidebar-input__error-message"
+          >Это поле обязательно</span
+        >
+      </div>
+
+      <div class="sidebar-input__item">
+        <label for="about">Расскажи немного о себе</label>
+        <Textarea
+          autoResize
+          rows="1"
+          class="sidebar-input__field"
+          :class="{ 'input-error': errorsStaff.about }"
+          id="about"
+          v-model="fieldsStaff.about"
+        />
+        <span v-if="errorsStaff.about" class="sidebar-input__error-message"
+          >Это поле обязательно</span
+        >
+      </div>
+      <FileUploadCustom title="Прикрепить резюме" />
+    </div>
+
+    <div class="sidebar-footer">
+      <GradientButton title="Оставить заявку" @click="submitStaff" />
+      <span class="sidebar-footer__privacy">
+        Нажимая кнопку, вы соглашаетесь с нашей
+        <a class="sidebar-footer__privacy-link" href="#">
+          политикой конфиденциальности
+        </a>
+      </span>
+    </div>
+  </div>
+</template>
---
+++ components/UI-kit/FileUploadCustom.vue
@@ -0,0 +1,175 @@
+<script setup>
+import FileUpload from "primevue/fileupload";
+
+defineProps({
+  title: {
+    type: String,
+    default: null,
+  },
+});
+
+const formatFileSize = (size) => {
+  const i = Math.floor(Math.log(size) / Math.log(1024));
+  return (
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    " " +
+    ["B", "KB", "MB", "GB", "TB"][i]
+  );
+};
+
+const handleFileSelect = (event) => {
+  event.files.splice(0, event.files.length);
+  event.files.push(event.originalEvent.target.files[0]);
+};
+</script>
+
+<template>
+  <FileUpload
+    :multiple="false"
+    @select="handleFileSelect"
+    :pt="{
+      input: { class: 'file-upload' },
+      buttonbar: { class: 'buttonbar-file-upload' },
+      content: { class: 'content-file-upload' },
+      thumbnail: { class: 'thumbnail-file-upload' },
+      details: { class: 'details-file-upload' },
+      file: { class: 'file-file-upload' },
+      fileSize: { class: 'file-size-file-upload' },
+    }"
+  >
+    <template
+      #header="{
+        files,
+        uploadedFiles,
+        chooseCallback,
+        uploadCallback,
+        clearCallback,
+      }"
+    >
+      <div :class="[{ 'file-upload-selected': files.length > 0 }]">
+        <button
+          :class="[
+            'file-upload-button',
+            { 'icon-paper-clip': files.length === 0 },
+            { 'file-upload-button_selected': files.length > 0 },
+          ]"
+          @click="chooseCallback"
+        >
+          {{ files.length > 0 ? files[0].name : title }}
+
+          <span v-if="files.length > 0" class="file-size">
+            {{ formatFileSize(files[0].size) }}
+          </span>
+        </button>
+
+        <button
+          v-if="files.length > 0"
+          @click="clearCallback"
+          class="close-button icon-close"
+        ></button>
+      </div>
+    </template>
+  </FileUpload>
+</template>
+
+<style lang="scss">
+@import "../../assets/scss/helpers/fonts-mixin";
+
+.file-size {
+  @include font-link-1;
+}
+
+.content-file-upload {
+  display: none;
+}
+
+.file-upload {
+  display: none;
+}
+
+.buttonbar-file-upload {
+  display: flex;
+}
+
+.file-file-upload {
+  display: flex;
+}
+
+.thumbnail-file-upload {
+  display: none;
+}
+
+.details-file-upload {
+  display: flex;
+}
+
+.file-size-file-upload {
+  margin-left: 5px;
+}
+
+.file-upload-selected {
+  display: flex;
+  border: none;
+  border-radius: 12px;
+  padding: 0 9px 0 14px;
+  background-color: var(--color-grey-light);
+}
+
+.file-upload-button {
+  @include font-link-1;
+
+  display: flex;
+  text-align: center;
+  align-items: center;
+  gap: 4px;
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  padding: 14px 0;
+  color: var(--color-black-subtitle);
+
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+}
+
+.icon-paper-clip {
+  &:after {
+    content: "\0048";
+    font-size: 15px;
+    color: var(--color-black-subtitle);
+    font-weight: inherit;
+  }
+}
+
+.close-button {
+  display: flex;
+  align-items: center;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  padding: 0 5px;
+}
+
+.icon-close {
+  &:after {
+    content: "\004A";
+    font-size: 12px;
+    color: var(--color-black-subtitle);
+    font-weight: inherit;
+  }
+}
+
+.file-upload-button_selected {
+  padding-right: 20px;
+}
+
+@media (max-width: 800px) {
+  .icon-paper-clip {
+    &:after {
+      font-size: 12px;
+    }
+  }
+}
+</style>
