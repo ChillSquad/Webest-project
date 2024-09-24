daf4c8b92ff9ba7413b4c26f2db761c1281fc6f8
tunka8090 <tunka8090@yandex.ru>
2024-09-24 09:30:44 +0300

    [upd]: Добавление навигации

--- components/CasePage/CaseUnit.vue
+++ components/CasePage/CaseUnit.vue
@@ -5,32 +5,41 @@ import CaseUnitCard from "./CaseUnitCard.vue";
 const { data: casesData } = await useAsyncData("cases", () => {
   return $fetch("/api/cases/", { method: "GET" });
 });
+
+defineProps({
+  title: {
+    type: Boolean,
+    default: true,
+  },
+  slice: {
+    type: Number,
+    default: 8,
+  },
+});
 </script>
 
 <template>
   <div class="case-unit">
     <div class="container">
-      <div class="case-unit__inner">
-        <div class="case-unit__heading">
-          <TitleButton title="кейсы" />
-        </div>
-        <ul class="case-unit__table-list">
-          <li
-            v-for="(caseItem, index) in casesData"
-            :key="index"
-            :class="['case-unit__table-item', { wide: caseItem.wide }]"
-          >
-            <CaseUnitCard
-              :urlImage="`images/imageCase${index + 1}.png`"
-              :title="caseItem.title"
-              :hasData="caseItem.plate"
-              :moreData="caseItem.moreData"
-              :textColor="caseItem.textColor"
-              :wide="caseItem.wide"
-            />
-          </li>
-        </ul>
+      <div v-if="title" class="case-unit__heading">
+        <TitleButton title="кейсы" route="/case" />
       </div>
+      <ul class="case-unit__table-list">
+        <li
+          v-for="(caseItem, index) in casesData.slice(0, slice)"
+          :key="index"
+          :class="['case-unit__table-item', { wide: caseItem.wide }]"
+        >
+          <CaseUnitCard
+            :urlImage="`images/imageCase${index + 1}.png`"
+            :title="caseItem.title"
+            :hasData="caseItem.plate"
+            :moreData="caseItem.moreData"
+            :textColor="caseItem.textColor"
+            :wide="caseItem.wide"
+          />
+        </li>
+      </ul>
     </div>
   </div>
 </template>
